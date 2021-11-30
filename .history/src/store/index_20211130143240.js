dfa79546b84098e486919c73ebb48f43bcc0da58
Hieu.Mai <xuanhieu26295@gmail.com>
2021-11-30 14:50:41 +0700

    add file

---
+++ .history/src/store/index_20211130143240.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    sampleBlogCards: [
+      { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021"},
+      { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021"},
+      { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021"},
+      { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021"},
+    ],
+    editPost: null,
+    user: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUsername: null,
+    profileId: null,
+    profileIniitials: null,
+  },
+  mutations: {
+    toggleEditPost(state, payload){
+      state.editPost = payload;
+      console.log(state.editPost);
+    },
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
